4640899588b5bc631d0341541b157706d1c6a34a
Diogo Guedes <diogo.f.guedes@parceiros.nos.pt>
2026-01-09 17:57:27 +0000

    feat: serverless python func update, mp3 file

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -13,3 +13,11 @@ resource "google_storage_bucket" "output_bucket" {
   uniform_bucket_level_access = true
   public_access_prevention    = "enforced"
 }
+resource "google_storage_bucket_object" "mp3" {
+
+  name         = "file.mp3"
+  bucket       = google_storage_bucket.input_bucket.name
+  source       = "assets/file.mp3"
+  content_type = "audio/mpeg"
+
+}
