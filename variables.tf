8bd7cbb417e125125584e189224fc5df91969521
Diogo Guedes <diogo.f.guedes@parceiros.nos.pt>
2026-01-09 17:25:24 +0000

    feat: init project serverless voice transcript with google provider

---
+++ variables.tf
@@ -0,0 +1,10 @@
+
+variable "project_id" {
+  description = "The ID of the GCP project to deploy resources into."
+  type        = string
+}
+
+variable "region" {
+  description = "The region to deploy resources into."
+  type        = string
+}
